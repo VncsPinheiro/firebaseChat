26ebaec41082373c9ca3b7ea89fb06b71b34e89a
Silas <dev.silas.freitas@outlook.com>
2025-04-04 10:45:40 -0300

    aa

---
+++ components/Loading.js
@@ -0,0 +1,17 @@
+//Importação de componentes básicos do React Native
+import { View, Text } from 'react-native'
+//Importação React 
+import React from 'react'
+//Importação da biblioteca Lottie para animações
+//Lottie permite usar animações usar animações JSON criadas no After Effects/Figma
+import LottieView from 'lottie-react-native';
+
+/** 
+    * Componente Loading
+    *
+    * Este componente renderiza uma animação de carregamento usando Lottie
+    * 
+    * @param {number} size - Tamanho do indicador de carregamento (altura em unidades de medida do React Native)
+    * @returns {JSX.Element} Componente de Loading com animação
+*/
+export xsxs
