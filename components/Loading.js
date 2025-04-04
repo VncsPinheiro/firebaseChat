2090126fe67dca8377f7864652a0d2159bb690e1
Silas <dev.silas.freitas@outlook.com>
2025-04-04 10:52:08 -0300

    end

--- components/Loading.js
+++ components/Loading.js
@@ -14,4 +14,19 @@ import LottieView from 'lottie-react-native';
     * @param {number} size - Tamanho do indicador de carregamento (altura em unidades de medida do React Native)
     * @returns {JSX.Element} Componente de Loading com animação
 */
-export xsxs
+export default function Loading({size}) {
+    return (
+        //Container que envolve a animação
+        //USa a propriedade size para definir a altura
+        //aspectRatio 1 garante que o componente seja um quadrado perfeito (largura = altura)
+        <View style={{height: size, aspectRatio: 1}}>
+            {/*
+                Componente LottieView que exibe a animação
+                    style ={{flex:1}} faz com que a animação ocupe todo o espaço disponível no container source carrega o arquivo JSON da animação da pasta assets/images
+                    autoPlay inicia a animação automaticamente quando o componente é montado 
+                    loop faz com que a animação se repita indefinidamente
+                
+    */}
+        </View>
+    )
+}
